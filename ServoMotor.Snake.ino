b3872698c0a7b2e8ff170f137c652c8ceee66b3e
sgendron <sgendron@bancroftschool.org>
2018-05-08 14:27:12 -0400

    Add files via upload
    Completed the programming.

--- ServoMotor.Snake.ino
+++ ServoMotor.Snake.ino
@@ -41,6 +41,19 @@ void loop() {
     delay(10);
   }
 
-  digitalWrite
+  digitalWrite (trigPin, LOW) ;
+  delayMicroseconds (2) ;
+
+
+  digitalWrite (trigPin, HIGH) ;
+  delayMicroseconds (10) ;
+  digitalWrite (trigPin, LOW) ;
+
+
+  duration = pulseIn (echoPin, HIGH) ;
+  distance= duration*0.034/2;
+
+  Serial.print ("Distance: ");
+  Serial.println(distance) ;
 
 }
